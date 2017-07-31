28b389188fe383b892a12575bfa1118ae2bae628
xiaoting <liumengying@xiangongshe.com>
2017-07-31 14:49:40 +0800

    home swiper

--- pages/home/home.js
+++ pages/home/home.js
@@ -3,30 +3,12 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    productImages: [
+      { url: '/asset/images/personal-bg.png'},
+      { url: '/asset/images/head-portrait.png'}
+    ]
   },
-  //事件处理函数
-  // bindViewTap: function() {
-  //   wx.navigateTo({
-  //     url: '../logs/logs'
-  //   })
-  // },
-  // onLoad: function () {
-  //   console.log('onLoad')
-  //   var that = this
-  //   //调用应用实例的方法获取全局数据
-  //   app.getUserInfo(function(userInfo){
-  //     //更新数据
-  //     that.setData({
-  //       userInfo:userInfo
-  //     })
-  //   })
-  // },
-  // bindRegionChange: function (e) {
-  //   console.log('picker发送选择改变，携带值为', e.detail.value)
-  //   this.setData({
-  //     region: e.detail.value
-  //   })
-  // }
+  onLoad: function () {
+   
+  }
 })
