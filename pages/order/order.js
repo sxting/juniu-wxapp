ddb44f6b229129e017b3c928cd86c685e26903d8
xiaoting <liumengying@xiangongshe.com>
2017-08-04 18:56:12 +0800

    order time

--- pages/order/order.js
+++ pages/order/order.js
@@ -4,27 +4,34 @@ import { errDialog } from '../../utils/util';
 var app = getApp()
 Page({
   data: {
-    timeList: ['08:00', '08:00', '08:00', '08:00', '08:00', '08:00', '08:00', '08:00', '08:00', '08:00', '08:00', '08:00', '08:00', '08:00', '08:00', '08:00',],
     storeId: '1498113571836349700342',
     reserveType: '', //预约配置类型 MAN、PRODUCT、TIME 
     dateList: [],
-    startTime: '',
-    endTime: '',
+    timeList: [],
+    craftsmanId: '',
+    productId: '',
   },
   onLoad: function () {
-
     let today = changeDate.call(this,new Date());
+    let dateArr = [];
+    for(let i=0; i<7; i++) {
+      dateArr.push(getAfterSomeDay.call(this, today, i))
+    }
+    dateArr[0].week = '今天';
+    dateArr[1].week = '明天';
+    this.setData({
+      dateList: dateArr
+    })
 
-    let dateArr = getAfterSomeDay.call(this,today,7)  
-    
-    console.log(dateArr)
     reserveConfig.call(this)
   },
+
   onCraftsmanClick: function() {
     wx.navigateTo({
       url: '/pages/craftsman/select/select',
     })
   },
+
   onProductClick: function() {
     wx.navigateTo({
       url: '/pages/product/select/select',
@@ -41,13 +48,34 @@ function reserveConfig() {
 
   orderService.reserveConfig(data).subscribe({
     next: res => {
-      let data = res.data.data;
-      console.log(data);
+      console.log(res);
+      let todayDay = changeDate.call(this, new Date())
+      let timeArr = {
+        time: [],
+        timeShow: []
+      };
+
+      //生成时间start
+      let timeData = res.businessStart + '-' + res.businessEnd;
+      // let timeData = '09:00-19:30';
+      let timeDataArr = timeData.split('-');
+      let startTime = new Date(todayDay + ' ' + timeDataArr[0] + ':00').getTime();
+      let endTime = new Date(todayDay + ' ' + timeDataArr[1] + ':00').getTime();
+      for (let i = 0; i < ((endTime - startTime) / 1000 / 60 / 30); i++) {
+        timeArr.timeShow.push(new Date(new Date(todayDay + ' ' + timeDataArr[0] + ':00').getTime() + 30 * 60 * 1000 * i));
+        timeArr.time.push(new Date(new Date(todayDay + ' ' + timeDataArr[0] + ':00').getTime() + 30 * 60 * 1000 * i));
+      }
+      for (let i = 0; i < timeArr.timeShow.length; i++) {
+        timeArr.timeShow[i] = (timeArr.timeShow[i].getHours().toString().length > 1 ? timeArr.timeShow[i].getHours() : '0' + timeArr.timeShow[i].getHours()) + ':' +
+          (timeArr.timeShow[i].getMinutes().toString().length > 1 ? timeArr.timeShow[i].getMinutes() : '0' + timeArr.timeShow[i].getMinutes());
+      }
+      //生成时间end
+
       this.setData({
-        reserveType: data.reserveType,
-        startTime: data.businessStart,
-        endTime: data.businessEnd
+        reserveType: res.reserveType,
+        timeList: timeArr.timeShow
       })
+
     },
     error: err => errDialog(err),
     complete: () => wx.hideToast()
@@ -64,13 +92,13 @@ function getAfterSomeDay(date, num) {
   let dateNum = odate.getDate() < 10 ? '0' + odate.getDate() : odate.getDate();
   if ((odate.getMonth() + 1) >= 10) {
     return {
-      date: (odate.getMonth() + 1) + '-' + dateNum,
+      date: (odate.getMonth() + 1) + '.' + dateNum,
       year: odate.getFullYear(),
       week: changeDayToChinese.call(this,odate.getDay())
     };
   } else {
     return {
-      date: '0' + (odate.getMonth() + 1) + '-' + dateNum,
+      date: '0' + (odate.getMonth() + 1) + '.' + dateNum,
       year: odate.getFullYear(),
       week: changeDayToChinese.call(this,odate.getDay())
     };
