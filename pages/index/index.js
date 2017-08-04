abaa3e700211fb4cbbb05ac8c765f37b105813c5
xiejinbin <xiejinbin@me.com>
2017-08-04 18:55:33 +0800

    add js

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,7 +10,11 @@ Page({
     userInfo: {},
     provinceName: '北京',
     region: ['广东省', '广州市', '海珠区'],
-    webServeKey: '3e9b7b7b8d1511312c029f5bf45e1023'
+    webServeKey: '3e9b7b7b8d1511312c029f5bf45e1023',
+    // 门店选择列表
+    storeList: [],
+    // 屏幕高度
+    windowHeight: 0
   },
   //事件处理函数
   // bindViewTap: function() {
@@ -19,11 +23,19 @@ Page({
   //   })
   // },
   onLoad: function () {
+    let self = this;
     try {
       wx.setStorageSync(constant.TOKEN, '27f3733b5daeb3d89a53b6c561f5c753')
     } catch (e) {
     }
-    getStoreListInfo.call();
+    wx.getSystemInfo({
+      success: function (res) {
+        self.setData({
+          windowHeight: res.windowHeight
+        });
+      }
+    })
+    getStoreListInfo.call(this);
     wx.getSetting({
       success(res) {
         if (!res.authSetting['scope.userLocation']) {
@@ -74,9 +86,12 @@ Page({
 
 /**获取门店列表 */
 function getStoreListInfo() {
+  let self = this;
   indexService.getStoreList().subscribe({
     next: res => {
-      console.log(res);
+      this.setData({
+        storeList: res.content
+      })
     },
     error: err => errDialog(err),
     complete: () => wx.hideToast()
