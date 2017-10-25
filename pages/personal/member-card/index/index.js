d06575995c89d2e1a6bc2a2f6b74e355c3a35c0e
xiaoting <liumengying@xiangongshe.com>
2017-10-25 14:28:45 +0800

    member-card

---
+++ pages/personal/member-card/index/index.js
@@ -0,0 +1,13 @@
+//获取应用实例
+import { personalService } from '../../shared/service.js'
+import { errDialog } from '../../../../utils/util';
+import { constant } from '../../../../utils/constant'
+
+var app = getApp()
+Page({
+  data: {
+    
+  },
+  onLoad: function () {
+  }
+})
