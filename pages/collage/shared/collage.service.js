32e4c331bdaf55476db6ab83d1c9610f0e729192
xiaoting <994747304@qq.com>
2018-08-08 14:24:37 +0800

    service

--- pages/collage/shared/collage.service.js
+++ pages/collage/shared/collage.service.js
@@ -2,11 +2,11 @@ import { constant } from '../../../utils/constant';
 import { http } from '../../../utils/http';
 let collageService = {};
 
-let API = constant.apiUrl + '/pintuan-service';
+let API = constant.apiUrl;
 
 //商品详情
 collageService.getProductDetail = (data) => {
-  let apiUrl = API + '/consumer/pintuan/detail.json';
+  let apiUrl = API + '/consumer/activity/detail.json';
   return http.get(apiUrl, data);
 }
 
